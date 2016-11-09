7ef05ac1229910421970ea93016a2468d2a0d048
yousufahmed <youmed.tech@gmail.com>
2016-11-09 11:57:14 +0000

    Update Evaluation.java
    Working to improve evaluation

--- src/dme/Evaluation.java
+++ src/dme/Evaluation.java
@@ -1,19 +1,19 @@
 package dme;
 
-import engine.Board;
-import engine.Piece;
-import engine.PieceColor;
-import engine.PieceType;
-
 import java.util.ArrayList;
+
+import core.Board;
+import core.Piece;
+import core.PieceColor;
+import core.PieceType;
 
 public class Evaluation {
     private double evalMat(ArrayList<Piece> pieceList) {
-        int queenConst = 9;
-        int rookConst = 5;
-        int knightConst = 3;
-        int bishopConst = 3;
-        int pawnConst = 1;
+        double queenConst = 900;
+        double rookConst = 500;
+        double knightConst = 330;
+        double bishopConst = 320;
+        double pawnConst = 100;
         int queenW = Board.noOfPieces(pieceList, PieceType.QUEEN, PieceColor.WHITE);
         int queenB = Board.noOfPieces(pieceList, PieceType.QUEEN, PieceColor.BLACK);
         int rookW = Board.noOfPieces(pieceList, PieceType.ROOK, PieceColor.WHITE);
@@ -23,8 +23,7 @@ public class Evaluation {
         int bishopW = Board.noOfPieces(pieceList, PieceType.BISHOP, PieceColor.WHITE);
         int bishopB = Board.noOfPieces(pieceList, PieceType.BISHOP, PieceColor.BLACK);
         int pawnW = Board.noOfPieces(pieceList, PieceType.PAWN, PieceColor.WHITE);
-        int pawnB = Board.noOfPieces(pieceList, PieceType.PAWN, PieceColor.BLACK);
-
+        int pawnB = Board.noOfPieces(pieceList, PieceType.PAWN, PieceColor.BLACK);        
         double eval = queenConst * (queenW - queenB) + rookConst * (rookW - rookB)
                 + knightConst * (knightW - knightB) + bishopConst * (bishopW - bishopB) + pawnConst * (pawnW - pawnB);
         return eval;
@@ -44,9 +43,71 @@ public class Evaluation {
         }
         return mobilityFactor * (whiteMoves - blackMoves);
     }
-
+    private double developedScores(ArrayList<Piece> pieceList){
+    	double score = 0;
+        double queenConst = 1000;
+        double rookConst = 600;
+        double knightConst = 350;
+        double bishopConst = 400;
+        double pawnConst = 70;
+        for(Piece piece : pieceList){
+        	if(piece.getNumberOfMoves() != 0){
+        		switch(piece.getPieceType()){
+        			case QUEEN:
+        				score += queenConst;
+        				break;
+        			case ROOK:
+        				score += rookConst;
+        				break;
+        			case KNIGHT:
+        				score += knightConst;
+        				break;
+        			case BISHOP:
+        				score += bishopConst;
+        				break;
+        			case PAWN:
+        				score += pawnConst;
+        				break;
+        			default:
+        				score += 0;
+        				break;
+        		}
+        	}
+        }
+        return score;
+    }
+    private double advancedPawns(ArrayList<Piece> pieceList){
+    	double score = 0;
+    	for(Piece piece : pieceList){    		
+    		if(piece.getPieceType() == PieceType.PAWN){
+    			
+    		}
+    	}
+    	return score;
+    }
+    private double bishopPair(ArrayList<Piece> pieceList, PieceColor color){
+    	double bishopPairBonus = 50;
+    	int noOfBishops = Board.noOfPieces(pieceList, PieceType.BISHOP, color);
+    	return (noOfBishops == 2) ? bishopPairBonus : 0;
+    }
+    private double knightOnEdge(ArrayList<Piece> pieceList){
+    	double knightOnEdgePenalty = -50;
+    	double score = 0;
+    	for(Piece piece : pieceList){
+    		if(piece.getPieceType() == PieceType.KNIGHT){
+    			if(isKnightOnEdge(piece)){
+    				score += knightOnEdgePenalty;
+    			}
+    		}
+    	}
+    	return score;
+    }
+    
+    private boolean isKnightOnEdge(Piece piece){
+    	return (piece.getPos().getCol() == 0 || piece.getPos().getCol() == 7);
+    }
     private double evalPos(ArrayList<Piece> pieceList) {
-        double positionFactor = 150;
+        double positionFactor = 50;
         int whiteScore = 0;
         int blackScore = 0;
         for (Piece piece : pieceList) {
@@ -62,8 +123,8 @@ public class Evaluation {
     }
 
     public double totalEvaluation(ArrayList<Piece> pieceList, PieceColor pieceColor) {
-        // return (pieceColor.getColorFactor() * (evalMat(pieceList) + evalMob(pieceList))) + evalPos(pieceList);
-        return (pieceColor.getColorFactor() * (evalMat(pieceList) + evalMob(pieceList)));
+        return (pieceColor.getColorFactor() * (evalMat(pieceList) + evalMob(pieceList))) + evalPos(pieceList);
+        //return (pieceColor.getColorFactor() * (evalMat(pieceList) + evalMob(pieceList)));
     }
 
 
