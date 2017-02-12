3944a8719edd3fb57c7d95fa44479403cc220759
Yousuf Ahmed <youmed.tech@gmail.com>
2017-02-12 12:09:08 +0000

    Commented move class

--- src/core/Move.java
+++ src/core/Move.java
@@ -8,36 +8,46 @@ public class Move {
 	private boolean promotion;
 	private boolean enPassant;
 
+	// Move class used to store the crucial information about a particular move
 	public Move(int pieceType, int oldPosition, int finalPosition) {
 		this.oldPosition = oldPosition;
 		this.finalPosition = finalPosition;
 		this.pieceType = pieceType;
 	}
 
+	// Returns the index of the square where the piece will move to
 	public int getFinalPos() {
 		return finalPosition;
 	}
 
+	// Returns the id number of the piece making the move
 	public int getPieceType() {
 		return pieceType;
 	}
 
+	// Returns the index where the piece came from
 	public int getOldPos() {
 		return oldPosition;
 	}
 
+	// Returns the castling flag of the move
+	// So that it can be judged whether or not the move is a castling move
+	// Also what type of castling i.e. white queenside, black kingside etc.
 	public byte getCastlingFlag() {
 		return castling;
 	}
 
+	// Returns boolean to show whether or not move is a pawn promotion
 	public boolean isPromotion() {
 		return promotion;
 	}
 
+	// Returns boolean to show whether or not it is a pawn en passant attack
 	public boolean isEnPassant() {
 		return enPassant;
 	}
-
+	
+	//Setters for castling, promotion and en passant
 	public void setCastling(byte castling) {
 		this.castling = castling;
 	}
